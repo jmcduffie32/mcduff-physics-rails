0b32299a7200800ad2a6a378e8cf7eda300213e3
Jon McDuffie <bobber321988@gmail.com>
2015-04-11 19:45:53 -0400

    Added Mechanics Static Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,29 +2,36 @@ class StaticPagesController < ApplicationController
   attr_reader :page
   def home
     @page = 'main'
+    @title = @page.upcase
   end
 
   def contact
     @page = 'contact'
+    @title = @page.upcase
   end
 
   def ap1
     @page = 'ap1'
+    @title = @page.upcase
   end
 
   def mechanics
     @page = 'mechanics'
+    @title = @page.upcase
   end
 
   def emag
     @page = 'emag'
+    @title = @page.upcase
   end
 
   def ping_pong
     @page = 'main'
+    @title = "PING PONG"
   end
 
   def sci_oly
     @page = 'main'
+    @title = "SCI OLY"
   end
 end
