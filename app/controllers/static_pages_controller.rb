9f9b0e1282a08726bda73819615f0ccd072e82d1
Jon McDuffie <bobber321988@gmail.com>
2015-04-11 19:07:36 -0400

    Added ap1 static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,30 @@
 class StaticPagesController < ApplicationController
+  attr_reader :page
   def home
+    @page = 'main'
   end
 
   def contact
+    @page = 'contact'
   end
 
   def ap1
+    @page = 'ap1'
   end
 
   def mechanics
+    @page = 'mechanics'
   end
 
   def emag
+    @page = 'emag'
   end
 
   def ping_pong
+    @page = 'main'
   end
 
   def sci_oly
+    @page = 'main'
   end
 end
